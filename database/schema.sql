9b83c1594f483a4c8b9308b860443f44f3b376f9
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-09-08 02:22:38 +0000

    Created post codeand sql insert

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,82 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+
+set client_min_messages to warning;
+
+-- DANGER: this is NOT how to do it in the real world.
+-- `drop schema` INSTANTLY ERASES EVERYTHING.
+drop schema "public" cascade;
+
+create schema "public";
+
+CREATE TABLE "trips" (
+    "tripId" serial NOT NULL,
+    "startLat" double precision NOT NULL,
+    "startLng" double precision NOT NULL,
+    "stopLat" double precision NOT NULL,
+    "stopLng" double precision NOT NULL,
+    "topSpeed" integer NOT NULL,
+    "vehicleId" serial NOT NULL,
+    "startAddress" TEXT NOT NULL,
+    "stopAddress" TEXT NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL,
+    CONSTRAINT "trips_pk" PRIMARY KEY ("tripId")
+) WITH (
+  OIDS=FALSE
+);
+CREATE TABLE "vehicles" (
+    "vehicleId" serial NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL,
+    "name" TEXT NOT NULL,
+    "make" TEXT NOT NULL,
+    "model" TEXT NOT NULL,
+    "year" integer NOT NULL,
+    "speedAlert" integer NOT NULL,
+    "brakeAlert" BOOLEAN NOT NULL,
+    "accelerationAlert" BOOLEAN NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    "photo" TEXT NOT NULL,
+    CONSTRAINT "vehicles_pk" PRIMARY KEY ("vehicleId")
+) WITH (
+  OIDS=FALSE
+);
+CREATE TABLE "drivers" (
+    "driverId" serial NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    "vehicleId" serial NOT NULL,
+    CONSTRAINT "drivers_pk" PRIMARY KEY ("driverId")
+) WITH (
+  OIDS=FALSE
+);
+CREATE TABLE "users" (
+    "userId" serial NOT NULL,
+    "username" serial NOT NULL UNIQUE,
+    "hashedPassword" TEXT NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+CREATE TABLE "stops" (
+    "stopId" serial NOT NULL,
+    "stopLat" double precision NOT NULL,
+    "stopLng" double precision NOT NULL,
+    "stopAddress" TEXT NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL,
+    "vehicleId" serial NOT NULL,
+    "beginTime" TIMESTAMP NOT NULL,
+    "endTime" TIMESTAMP NOT NULL,
+    CONSTRAINT "stops_pk" PRIMARY KEY ("stopId")
+) WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "trips" ADD CONSTRAINT "trips_fk0" FOREIGN KEY ("vehicleId") REFERENCES "vehicles"("vehicleId");
+ALTER TABLE "drivers" ADD CONSTRAINT "drivers_fk0" FOREIGN KEY ("vehicleId") REFERENCES "vehicles"("vehicleId");
+ALTER TABLE "stops" ADD CONSTRAINT "stops_fk0" FOREIGN KEY ("vehicleId") REFERENCES "vehicles"("vehicleId");
--- server/index.js
+++ server/index.js
@@ -2,12 +2,32 @@ require('dotenv/config');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
+// const ClientError = require('./client-error');
 
 const app = express();
 
 app.use(staticMiddleware);
 
 app.use(errorMiddleware);
+
+// app.get('/vehicleinfo', (req, res, next) => {
+//   console.log('success');
+// });
+app.post('/vehicleinof', (req, res, next) => {
+  // const { name, make, model, year } = req.body;
+  // if (!name || !model) {
+  //   throw new ClientError(400, 'name and model are required fields');
+  // }
+  // const sql = `
+  //   insert into "vehicle" ("name", "make", "model", "year")
+  //       values ($1, $2, $3, $4)
+  //       returning *
+  // `;
+  // const params = [name, make, model, year];
+
+  // -make sql promise next
+
+});
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
