556fc458d2cddbf0dc605011fb4a11a848caf040
Ryan Skidmore <ryan.skidmore@yahoo.com>
2021-09-09 17:33:51 -0700

    Revert "Pins created on click with label. Route created on button click"

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-// import VehicleProfile from './vehicle-profile';
-import Routing from './routing';
+import VehicleProfile from './vehicle-profile';
 
 export default function Home(props) {
   return (
     <>
-    {/* <VehicleProfile /> */}
-      <Routing />
+    <VehicleProfile />
     </>
   );
 }
