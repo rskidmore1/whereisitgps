0d0238f791da48e3db10fe76c4351b4296971715
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-09-15 21:54:05 +0000

    Api call to sendtext made

--- client/pages/vehicle-alerts.jsx
+++ client/pages/vehicle-alerts.jsx
@@ -4,8 +4,9 @@ export default class VehicleAlerts extends React.Component {
   constructor(props) {
     super(props);
     const vId = 2;// Replace this when creating vehicle list feature
-    this.state = { vehicle: {}, vehicleId: vId };
+    this.state = { vehicle: {}, vehicleId: vId, demoVehicle: false };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.sendText = this.sendText.bind(this);
 
     fetch(`/api/vehicleinfo/${vId}`)
       .then(res => res.json())
@@ -17,31 +18,53 @@ export default class VehicleAlerts extends React.Component {
 
   }
 
-  handleSubmit(event) {
-
-    const vehicle = { name: event.target[0].value, make: event.target[2].value, model: event.target[4].value, year: event.target[1].value, color: event.target[3].value, plate: event.target[5].value };
-
-    fetch('/api/vehicleinfo/', {
+  sendText(text) {
+    // console.log(text);
+    fetch('/api/sendtext/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
 
       },
 
-      body: JSON.stringify(vehicle)
+      body: JSON.stringify(text)
     })
       .then(response => response.json())
       .catch(error => {
         console.error('Error:', error);
       });
+  }
+
+  handleSubmit(event) {
+
+    const alerts = { speedAlert: event.target[0].checked, speedingThreshold: event.target[1].value, brakeAlert: event.target[2].checked, accelerationAlert: event.target[3].checked, textAlert: event.target[4].checked, emailAlert: event.target[5].checked };
+    this.setState({ vehicle: alerts });
+    // //console.log(event.target[0].checked);
+    // console.log(this.state.vehicle.demo);
+    // console.log(alerts.textAlert);
+    if (this.state.vehicle.demo) {
+      if (alerts.textAlert) {
+        if (this.state.vehicle.topSpeed > alerts.speedingThreshold) {
+          const message = `${this.state.vehicle.name} has exceeded speeding threshold. Speed: ${alerts.topSpeed}`;
+          const text = { toNumber: '+19492664664', message: message };
+          this.sendText(text);
+
+        }
+        if (alert.emailAlert) {
+          // email alert code goes here. -make function
+        }
+      }
+
+    }
+
     event.preventDefault();
   }
 
   render() {
 
-    // const currentVehicle = Object.assign({}, this.state.vehicle);
-    // const { name, make, model, year, color, plate } = currentVehicle;
-
+    const currentVehicle = Object.assign({}, this.state.vehicle);
+    const { speedingThreshold, speedAlert, brakeAlert, accelerationAlert, textAlert, emailAlert } = currentVehicle;
+    // console.log(currentVehicle);
     return (
       <div>
         <div className="alerts-box font-regular blue-text ">
@@ -52,15 +75,15 @@ export default class VehicleAlerts extends React.Component {
                   <table>
                     <tbody>
                       <tr>
-                      <td><input className="check-box" type="checkbox"></input></td>
-                      <td><label>Speed </label><input type="text"></input><label> mph</label></td>
+                      <td><input className="check-box" type="checkbox" defaultChecked={speedAlert}></input></td>
+                      <td><label>Speed </label><input type="text" defaultValue={speedingThreshold}></input><label> mph</label></td>
                       </tr>
                       <tr>
-                      <td><input className="check-box" type="checkbox"></input></td>
+                      <td><input className="check-box" type="checkbox" defaultChecked={brakeAlert} ></input></td>
                       <td><label>Hard Braking</label></td>
                       </tr>
                       <tr>
-                      <td><input className="check-box" type="checkbox"></input></td>
+                      <td><input className="check-box" type="checkbox" defaultChecked={accelerationAlert}></input></td>
                       <td><label>Hard Acceleration</label></td>
                       </tr>
                       <tr></tr>
@@ -73,11 +96,11 @@ export default class VehicleAlerts extends React.Component {
                   <table>
                     <tbody>
                     <tr>
-                      <td><input className="check-box" type="checkbox"></input></td>
+                      <td><input className="check-box" type="checkbox" defaultChecked={textAlert}></input></td>
                       <td><label>Text</label></td>
                     </tr>
                     <tr>
-                      <td><input className="check-box" type="checkbox"></input></td>
+                      <td><input className="check-box" type="checkbox" defaultChecked={emailAlert}></input></td>
                       <td><label>Email</label></td>
                     </tr>
 
